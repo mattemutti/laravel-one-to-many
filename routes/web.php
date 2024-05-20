0cc31ab63f20d610d7af2bfcf21f376cd2f4df24
mattemutti <mattemutti@gmail.com>
2024-05-20 17:56:57 +0200

    update resource route in the file web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,9 @@ Route::middleware(['auth', 'verified'])
 
 
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+        Route::resource('projects', ProjectController::class);
+
     });
 
 
