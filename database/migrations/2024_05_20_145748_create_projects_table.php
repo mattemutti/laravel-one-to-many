a554f0d36cee39848d213555f1055124d3113692
mattemutti <mattemutti@gmail.com>
2024-05-20 17:44:14 +0200

    update seeder, migration, and create project with faken on db

--- database/migrations/2024_05_20_145748_create_projects_table.php
+++ database/migrations/2024_05_20_145748_create_projects_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150);
+            $table->string('slug', 150);
+            $table->text('description')->nullable();
+            $table->date('create_data')->nullable();
+            $table->string('repo', 255)->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->title = $faker->words(2, true);
+            $project->slug = $faker->word();
+            $project->description = $faker->text(400);
+            $project->create_data = $faker->date('Y_m_d');
+            $project->repo = $faker->url();
+            $project->save();
+        }
     }
 }
