d8ae32c06d1f67362e66d27ad9af4b9baf84318f
mattemutti <mattemutti@gmail.com>
2024-05-23 15:58:02 +0200

    updat model file and create the relations with tables

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'cover_image', 'slug', 'description', 'create_data', 'repo', 'code', 'video'];
+    protected $fillable = ['title', 'cover_image', 'slug', 'description', 'create_data', 'repo', 'code', 'video', 'type_id'];
+
+    /**
+     * Get the user that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Type extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'slug'];
+
+    /**
+     * Get all of the Projects for the Type
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function Projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
