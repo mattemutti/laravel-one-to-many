b6cedf8b80a5211cc835a354ccfa0072438a8bc5
mattemutti <mattemutti@gmail.com>
2024-05-24 15:24:55 +0200

    add column in the types table

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -3,8 +3,10 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col">
-                @dd($type)
+            <div class="col text-danger text-center">
+
+                <h1>{{ $type->name }}</h1>
+                <h1>{{ $type->description }}</h1>
             </div>
         </div>
     </div>
