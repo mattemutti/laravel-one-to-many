84bfa1307149aae74db8c11766bf1c7fa59a3cbf
mattemutti <mattemutti@gmail.com>
2024-05-25 17:58:08 +0200

    update show page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,32 +5,48 @@
         @include('partials.message-confirm')
 
 
-        <div class="row py-4 gap-2 ">
-            <div class="col-6 py-5 text-center py-4">
-                <div class="card ">
-                    <div class="card-title p-4">ID: {{ $project->id }}</div>
-                    <div class="card-image">
-                        @if (Str::startsWith($project->cover_image, 'https://'))
-                            <img width="600" loading="lazy" src="{{ $project->cover_image }}" alt="{{ $project->title }}">
-                        @else
-                            <img width="600" loading="lazy" src="{{ asset('storage/' . $project->cover_image) }}"
-                                alt="{{ $project->title }}">
-                        @endif
+        <div class="row py-4">
+            <div class=" text-center py-4">
+                <div class="d-flex flex-column">
+
+                    <div class="d-flex py-5">
+                        <div class="d-flex text-white">Title:</div>
+                        <div class="d-flex text-white">{{ $project->title }}</div>
+                        <div class="text-white"></div>
                     </div>
-                    <div class="card-body">Title: {{ $project->title }}</div>
-                    <div class="card-body">Slug: {{ $project->slug }}</div>
-                    <div class="metadata">Type: {{ $project->type ? $project->type->name : 'No-type' }}</div>
-                    <div class="card-body">Create: {{ $project->create_data }}</div>
+                    <div class="d-flex  justify-content-end">
+                        <div class="text-white py-4">ID: {{ $project->id }}</div>
+                    </div>
+                    <div class="d-flex justify-content-start">
+                        <div class="">
+                            @if (Str::startsWith($project->cover_image, 'https://'))
+                                <img width="600" loading="lazy" src="{{ $project->cover_image }}"
+                                    alt="{{ $project->title }}">
+                            @else
+                                <img width="600" loading="lazy" src="{{ asset('storage/' . $project->cover_image) }}"
+                                    alt="{{ $project->title }}">
+                            @endif
+                        </div>
+                    </div>
+                    <div class="py-5">
+                        <div class="d-flex justify-content-end text-white">Description:</div>
+                        <div class="d-flex justify-content-end text-white"> {{ $project->description }}</div>
+                    </div>
+                    <div class="d-flex py-5">
+                        <div class="text-white">Slug: {{ $project->slug }}</div>
+                    </div>
+                    <div class="d-flex justify-content-evenly py-5">
+                        <div class="text-white">Repo: {{ $project->repo }}</div>
+                        <div class="text-white">Code: {{ $project->code }}</div>
+
+                    </div>
+                    <div class="text-white">Type: {{ $project->type ? $project->type->name : 'No-type' }}</div>
+                    <div class="text-white">Create: {{ $project->create_data }}</div>
                 </div>
-                <div class="card">
-                    <div class="card-body">Repo: {{ $project->repo }}</div>
-                    <div class="card-body">Code: {{ $project->code }}</div>
-                </div>
-                <div class="card">
-                    <div class="card-body">Video: {{ $project->description }}</div>
-                </div>
-                <div class="card">
-                    <div class="card-body">Description: {{ $project->description }}</div>
+
+                <div class="d-flex justify-content-center py-5">
+                    <div class="text-white">Video:</div>
+                    <div class="text-white">{{ $project->description }}</div>
                 </div>
             </div>
         </div>
